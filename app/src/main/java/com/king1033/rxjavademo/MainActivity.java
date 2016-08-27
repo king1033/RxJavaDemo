0ae23cf7e1345f5ed48e45df7fb5f1ea452f065e
King1033 <1786551559@qq.com>
2016-08-27 16:12:10 +0800

    add demo.

--- app/src/main/java/com/king1033/rxjavademo/MainActivity.java
+++ app/src/main/java/com/king1033/rxjavademo/MainActivity.java
@@ -2,12 +2,98 @@ package com.king1033.rxjavademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+
+/**
+ * RxJava的基本使用
+ * 步骤：
+ * 1、导包：compile 'io.reactivex:rxjava:1.1.9'
+ * 2、创建一个观察者对象
+ * 3、创建一个被观察者对象
+ * 4、建立观察者对象与被观察者对象之间的关联关系
+ */
 public class MainActivity extends AppCompatActivity {
+    Subscriber<String>subscriber;
+    Observable<String>observable;
+    private String TAG = "King1033";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // method1();
+        method2();
+    }
+
+    private void method2() {
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+
+            }
+        })
+                .observeOn(Schedulers.newThread())//表示观察者运行在子线程中
+                .subscribe(new Subscriber<Object>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        Log.i(TAG, "onNext: " + o);
+                        Log.i(TAG, "call1111: " + Thread.currentThread().getName());
+                    }
+                });
+
+
+    }
+
+    private void method1() {
+        //观察者对象
+        subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG, "subscriber ----->  onNext: "+Thread.currentThread().getName());
+
+            }
+        };
+
+        //被观察者对象
+        observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("ZhangSan");
+                Log.i(TAG, "Observable--->onNext: " + Thread.currentThread().getName());
+            }
+        });
+    }
+
+    public void click(View view){
+       //关联
+        observable.subscribeOn(Schedulers.newThread())
+                  .observeOn(Schedulers.newThread())//开启一个子线程
+                  .subscribe(subscriber);
     }
 }
